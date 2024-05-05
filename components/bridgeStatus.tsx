6bea3786f6e7e522ac2785c06137a6c1b1b33004
Alistair Singh <alistair.singh7@gmail.com>
2024-05-05 02:24:22 +0200

    more polish

--- components/bridgeStatus.tsx
+++ components/bridgeStatus.tsx
@@ -4,6 +4,7 @@ import { FC } from "react"
 import { Button } from "./ui/button"
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card"
 import Link from "next/link"
+import { LucideBarChart } from "lucide-react"
 
 export const BridgeStatus: FC = () => {
   return (
@@ -13,10 +14,7 @@ export const BridgeStatus: FC = () => {
       </HoverCardTrigger>
       <HoverCardContent className="w-auto">
         <div className="flex place-items-center space-x-4">
-          {/*<Avatar>
-            <AvatarImage src="https://github.com/vercel.png" />
-            <AvatarFallback>Delayed</AvatarFallback>
-          </Avatar>*/}
+          <LucideBarChart />
           <div className="space-y-1">
             <p>To Polkadot: <span className="text-green-700">Normal 10 min 5 secs</span></p>
             <p>To Ethereum: <span className="text-red-700">Delayed 32 min 6 secs</span></p>
