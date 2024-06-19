eb90747992745fcf236de88738a2d6750618209e
Alistair Singh <alistair.singh7@gmail.com>
2024-06-19 21:17:16 +0200

    banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ContextComponent } from "@/components/contextComponent";
 import { Footer } from "@/components/footer";
 import { Menu } from "@/components/menu";
 import { Toaster } from "@/components/ui/sonner";
-import { WalletNotice } from "@/components/walletNotice";
+import { Marquee } from "@/components/marquee";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Provider } from "jotai";
@@ -33,49 +33,53 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.className)}>
         <Provider>
-          <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-24">
-            <div className="w-full max-w-5xl md:gap-4 flex flex-col">
-              <div className="w-full place-items-center justify-between flex flex-col md:flex-row">
-                <div className="flex mb-4 lg:mb-0">
-                  <Image
-                    className="mb-2"
-                    src="/icon.svg"
-                    width={32}
-                    height={32}
-                    alt="Smiling bridge"
-                  />
-                  <h1 className="text-3xl font-semibold lg:text-4xl px-2">
-                    Snowbridge
-                  </h1>
+          <main>
+            <div>
+              <Marquee pauseOnHover={true}>
+                <div>
+                  ⚠️ This is an early release of Snowbridge. We are testing
+                  heavily over the next 2 weeks and do not recommend sending
+                  large amounts just yet. ⚠️
                 </div>
-                <Menu />
-              </div>
-              <div className="flex-col py-2 gap-2">
-                <div className="flex justify-center">
-                  <BridgeStatus />
+                <div>
+                  You may not see your bridged assets in your Wallet yet -
+                  support in SubWallet, Talisman and Nova is coming soon.
+                  Meanwhile, you can check your balance on this page.
                 </div>
-                <div
-                  className={cn(
-                    "flex justify-center",
-                    (
-                      process.env.NEXT_PUBLIC_SHOW_WALLET_NOTICE ?? "true"
-                    ).toLowerCase() === "true"
-                      ? "visible"
-                      : "invisible",
-                  )}
-                >
-                  <WalletNotice />
-                </div>
-              </div>
+              </Marquee>
             </div>
-            <div className="w-full max-w-5xl flex place-content-center">
-              <ContextComponent>{children}</ContextComponent>
-            </div>
-            <div className="w-full max-w-5xl flex flex-col place-items-center text-sm">
-              <Footer />
+            <div className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-24">
+              <div className="w-full max-w-5xl md:gap-4 flex flex-col">
+                <div className="w-full place-items-center justify-between flex flex-col md:flex-row">
+                  <div className="flex mb-4 lg:mb-0">
+                    <Image
+                      className="mb-2"
+                      src="/icon.svg"
+                      width={32}
+                      height={32}
+                      alt="Smiling bridge"
+                    />
+                    <h1 className="text-3xl font-semibold lg:text-4xl px-2">
+                      Snowbridge
+                    </h1>
+                  </div>
+                  <Menu />
+                </div>
+                <div className="flex-col py-2 gap-2">
+                  <div className="flex justify-center">
+                    <BridgeStatus />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full max-w-5xl flex place-content-center">
+                <ContextComponent>{children}</ContextComponent>
+              </div>
+              <div className="w-full max-w-5xl flex flex-col place-items-center text-sm">
+                <Footer />
+              </div>
+              <Toaster />
             </div>
           </main>
-          <Toaster />
         </Provider>
       </body>
     </html>
