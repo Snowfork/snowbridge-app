968941e466e463b47725091e1430831b989900e1
Alistair Singh <alistair.singh7@gmail.com>
2024-06-20 04:07:27 +0200

    better messages

--- components/transfer.tsx
+++ components/transfer.tsx
@@ -250,7 +250,7 @@ const userFriendlyErrorMessage = (
       error.code == toPolkadot.SendValidationCode.BeneficiaryAccountMissing &&
       formData.destination === "assethub"
     ) {
-      return "Beneficiary does not hold existential deposit on destination. Already have DOT on Polkadot? Teleport DOT to Asset Hub using your wallet.";
+      return "Beneficiary does not hold existential deposit on destination. Already have DOT on Polkadot? Teleport DOT to the beneficiary address on Asset Hub using your wallet.";
     }
     if (error.code == toPolkadot.SendValidationCode.InsufficientFee) {
       return "Insufficient ETH balance to pay transfer fees.";
@@ -258,7 +258,7 @@ const userFriendlyErrorMessage = (
     return error.message;
   } else if (error.kind === "toEthereum") {
     if (error.code == toEthereum.SendValidationCode.InsufficientFee) {
-      return "Insufficient DOT balance to pay transfer fees. Already have DOT on Polkadot? Teleport DOT to Asset Hub using your wallet.";
+      return "Insufficient DOT balance to pay transfer fees. Already have DOT on Polkadot? Teleport DOT to source address on Asset Hub using your wallet.";
     }
     return error.message;
   }
