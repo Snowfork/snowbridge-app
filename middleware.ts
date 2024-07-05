8d9d1b69290d54c606f8cfec337a48c969a00b61
Alistair Singh <alistair.singh7@gmail.com>
2024-07-05 04:04:36 +0200

    added final set of blocks

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,19 @@ import type { NextRequest } from "next/server";
 import { geolocation, ipAddress } from "@vercel/edge";
 import { NextResponse } from "next/server";
 
-const BLOCKED_COUNTRIES = ["US"];
+const BLOCKED_COUNTRIES = [
+  "CU", // Cuba
+  "IR", // Iran
+  "KP", // North Korea
+  "SY", // Syria
+  "RU", // Russia
+];
+
+const BLOCKED_REGIONS = [
+  { country: "UA", region: "14" }, // Ukraine Donetsk
+  { country: "UA", region: "43" }, // Ukraine Crimea
+  { country: "UA", region: "09" }, // Ukraine Luhansk
+];
 
 export const config = {
   matcher: "/((?!api|_next/static|not-found|blocked|favicon.ico).*)",
@@ -13,12 +25,30 @@ export function middleware(req: NextRequest) {
   const ip = ipAddress(req) || req.ip;
   // If country is unknown we default to US.
 
-  const country = geo.country ?? "US";
-  console.log("!!!!!!!!!!!!!!!!!!!!!!! remove this log", ip, geo);
+  const country = geo.country ?? "unknown";
+  const region = geo.countryRegion ?? "unknown";
 
-  // Specify the correct pathname
-  if (BLOCKED_COUNTRIES.includes(country) || true) {
-    console.log("Country blocked", country);
+  console.log("!!!!!!!!!!!!!!!!!!!!!!! remove this log", geo);
+
+  let blocked = false;
+
+  // Block countries
+  if (BLOCKED_COUNTRIES.includes(country)) {
+    console.log("Country blocked", geo);
+    blocked = true;
+  }
+
+  // Block regions
+  if (
+    BLOCKED_REGIONS.find(
+      (x) => x.country === country && x.region === region,
+    ) !== undefined
+  ) {
+    console.log("Region blocked", geo);
+    blocked = true;
+  }
+
+  if (blocked) {
     req.nextUrl.pathname = "/blocked";
     return NextResponse.rewrite(req.nextUrl, { status: 451 });
   }
