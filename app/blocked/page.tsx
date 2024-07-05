d27ac8b39390041f5e1c8c483b4249797ad3b5b8
Alistair Singh <alistair.singh7@gmail.com>
2024-07-05 03:26:34 +0200

    block all

--- app/blocked/page.tsx
+++ app/blocked/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Blocked() {
   return (
     <div className="flex-col gap-2 ">
-      <p>Page Not Found</p>
-      <div className="flex w-full justify-center">
-        <Link className="text-xs underline" href="/">
-          Go Home
-        </Link>
-      </div>
+      <p>Country blocked due to legal reasons.</p>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
 import type { NextRequest } from "next/server";
-
+import { geolocation, ipAddress } from "@vercel/edge";
 import { NextResponse } from "next/server";
 
-// Block Austria, prefer Germany
-const BLOCKED_COUNTRY = "AT";
+const BLOCKED_COUNTRIES = ["US"];
 
-// Limit middleware pathname config
 export const config = {
-  matcher: "/",
+  matcher: "/((?!api|_next/static|not-found|blocked|favicon.ico).*)",
 };
 
 export function middleware(req: NextRequest) {
-  // Extract country
-  const country = req.geo?.country || "US";
-  console.log("bad log", req.ip || "no ip", country);
+  const geo = req.geo ?? geolocation(req);
+  const ip = req.ip ?? ipAddress(req);
+  // If country is unknown we default to US.
+
+  const country = geo.country ?? "US";
+  console.log("!!!!!!!!!!!!!!!!!!!!!!! remove this log", ip, geo);
 
   // Specify the correct pathname
-  if (country === BLOCKED_COUNTRY) {
+  if (BLOCKED_COUNTRIES.includes(country) || true) {
+    console.log("Country blocked", country);
     req.nextUrl.pathname = "/blocked";
+    return NextResponse.rewrite(req.nextUrl, { status: 451 });
   }
-
-  // Rewrite to URL
-  return NextResponse.rewrite(req.nextUrl);
 }
