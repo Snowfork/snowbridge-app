9cf8d636602f20459f9de6fd9f523c589f48acf0
kilted-andres <andres@kilt.io>
2024-09-02 11:50:22 +0200

    feat: failed placement of parachain configs populater

--- store/snowbridge.ts
+++ store/snowbridge.ts
@@ -1,4 +1,10 @@
-import { getEnvironment, getEnvironmentName } from "@/lib/snowbridge";
+import {
+  getEnvironment,
+  getEnvironmentName,
+  parachainConfigs,
+  populateParachainConfigs,
+} from "@/lib/snowbridge";
+import { printify } from "@/utils/printify";
 import { Context, assets } from "@snowbridge/api";
 import { atom } from "jotai";
 
@@ -9,4 +15,12 @@ export const assetErc20MetaDataAtom = atom<{
 export const snowbridgeContextAtom = atom<Context | null>(null);
 
 export const snowbridgeEnvNameAtom = atom((_) => getEnvironmentName()); // this one is unnecessary. snowbridgeEnvironmentAtom.name can be used instead
-export const snowbridgeEnvironmentAtom = atom((_) => getEnvironment());
+export const snowbridgeEnvironmentAtom = atom(async () => {
+  await populateParachainConfigs();
+  console.log(
+    "Getting environment after adding this parachain configs: ",
+    printify(parachainConfigs),
+  );
+
+  return getEnvironment();
+});
