6da016f8526c4b12382627ea2a94f02740741896
Alistair Singh <alistair.singh7@gmail.com>
2025-01-23 12:31:33 +0200

    fix link (#50)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export function Footer() {
         <a className="footer-item" href="https://github.com/Snowfork/snowbridge-app/issues/new/choose" target="_blank">Report
           an issue</a>
         <a className="footer-item" href="https://docs.snowbridge.network/" target="_blank">Docs</a>
-        <a className="footer-item" href="https://dune.com/substrate/snowbridgee" target="_blank">Dune Dashboard</a>
+        <a className="footer-item" href="https://dune.com/substrate/snowbridge" target="_blank">Dune Dashboard</a>
       </div>
       <div className="text-xs py-2">
         Copyright © Snowfork 2024
