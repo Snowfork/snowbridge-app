8baeb86fd46488698f029d415c9de3b9a48aff8c
Alistair Singh <alistair.singh7@gmail.com>
2025-02-25 23:01:48 +0200

    fix refresh interval

--- app/history/api/route.ts
+++ app/history/api/route.ts
@@ -1,14 +1,14 @@
 // Next.js serverless options
 export const fetchCache = "default-no-store"; // Dont cache fetches unless asked.
 export const dynamic = "force-dynamic"; // Always run dynamically
-export const revalidate = 120; // Keep cache for 2 minutes
+export const revalidate = 30; // Keep cache for 2 minutes
 export const maxDuration = 90; // Timout adter
 
 import { getErrorMessage, getTransferHistoryV2 } from "@/lib/snowbridge";
 import { NextResponse } from "next/server";
 import { unstable_cache } from "next/cache";
 
-const CACHE_REVALIDATE_IN_SECONDS = 30 * 1000; // 30 seconds
+const CACHE_REVALIDATE_IN_SECONDS = 30; // 30 seconds
 
 const getCachedTransferHistory = unstable_cache(
   () => {
