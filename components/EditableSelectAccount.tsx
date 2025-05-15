c00db15816f2383a253abc9c2382e6067a4d676b
ron <yrong1997@gmail.com>
2025-05-15 13:31:14 +0000

    Fix component

--- components/EditableSelectAccount.tsx
+++ components/EditableSelectAccount.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { trimAccount } from "@/utils/formatting";
-import { FC, useState, useRef, SetStateAction } from "react";
+import { FC, useState, useRef, SetStateAction, useEffect } from "react";
 import Select, { components } from "react-select";
 
 const Input = (props: any) => <components.Input {...props} isHidden={false} />;
@@ -22,16 +22,10 @@ export const EditableSelectAccount: FC<SelectAccountProps> = ({
   const [value, setValue] = useState();
   const [inputValue, setInputValue] = useState("");
 
-  let displayAccounts = accounts.map((account) => {
-    return { label: account.key, value: account.key };
-  });
-
-  const options = useRef(displayAccounts).current;
-
   const onChange = (option: any) => {
-    setValue(option.value);
-    setInputValue(option ? option.label : "");
-    field.onChange(option.value);
+    setValue(option.key);
+    setInputValue(option ? option.key : "");
+    field.onChange(option.key);
   };
 
   const onInputChange = (
@@ -44,9 +38,17 @@ export const EditableSelectAccount: FC<SelectAccountProps> = ({
     }
   };
 
+  useEffect(() => {
+    // if the field is not set and there are accounts available, select the first account
+    if (!field.value && accounts.length > 0) {
+      field.onChange(accounts[0].key);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- watching for 'field' would introduce infinite loop
+  }, [accounts, field.value]);
+
   return (
     <Select
-      options={options}
+      options={accounts}
       isClearable={true}
       value={value}
       inputValue={inputValue}
