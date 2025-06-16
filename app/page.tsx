01d8f30173d17feddb11233a8296ac707ba3ca18
Alistair Singh <alistair.singh7@gmail.com>
2025-06-16 18:36:16 +0200

    move suspense

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,13 @@ import { TransferComponent } from "@/components/transfer/TransferComponent";
 import { ContextComponent } from "@/components/Context";
 import { MaintenanceBanner } from "@/components/MainenanceBanner";
 import { Suspense } from "react";
-import { LucideLoaderCircle } from "lucide-react";
 
 export default function Home() {
   return (
     <MaintenanceBanner>
       <ContextComponent>
-        <Suspense fallback={<Loading />}>
-          <TransferComponent />
-        </Suspense>
+        <TransferComponent />
       </ContextComponent>
     </MaintenanceBanner>
   );
 }
-
-const Loading = () => {
-  return (
-    <div className="flex text-primary underline-offset-4 hover:underline text-sm items-center">
-      Fetching Transfer Form{" "}
-      <LucideLoaderCircle className="animate-spin mx-1 text-secondary-foreground" />
-    </div>
-  );
-};
