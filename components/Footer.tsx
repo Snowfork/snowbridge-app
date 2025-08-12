56da309c3f990aefd09ccd6225ab81e8c28936f9
claravanstaden <claravanstaden64@gmail.com>
2025-08-12 09:07:48 +0200

    add powered by div

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,22 @@
 import { snowbridgeEnvNameAtom } from "@/store/snowbridge";
 import { acceptedTermsOfUseAtom } from "@/store/termsOfUse";
 import { useAtomValue, useSetAtom } from "jotai";
+import Image from "next/image";
 
 export function Footer() {
   const envName = useAtomValue(snowbridgeEnvNameAtom);
   const setAccepted = useSetAtom(acceptedTermsOfUseAtom);
   return (
     <>
+      <div className="text-sm mb-2 flex items-center gap-2">
+        Powered by{" "}
+        <Image
+          src="/images/polkadot_logo.png"
+          width={80}
+          height={20}
+          alt="Polkadot"
+        />
+      </div>
       <div className="text-xs mb-2">
         <a className="footer-item" onClick={() => setAccepted(false)}>
           Terms of Use
