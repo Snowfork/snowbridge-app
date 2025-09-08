e8acd2c0833eb869b4cd06a9e5dd8b297ca4d159
Alistair Singh <alistair.singh7@gmail.com>
2025-09-08 13:26:37 +0200

    fixed pending transactions

---
+++ utils/addresses.ts
@@ -0,0 +1,32 @@
+import { isHex, u8aToHex } from "@polkadot/util";
+import { decodeAddress, encodeAddress } from "@polkadot/util-crypto";
+
+export function walletTxChecker(
+  walletAccounts: string[] | null,
+): (sourceAddress: string, beneficiaryAddress: string) => boolean {
+  const accounts = new Set();
+  (walletAccounts ?? []).forEach((acc) => {
+    const address = acc.trim();
+    if (isHex(address, 160)) {
+      // 20 byte hex
+      // Add raw account
+      accounts.add(address.toLowerCase());
+      // Add account extended to 32byte address with zero pad. (Subsquid format for 20 byte address)
+      accounts.add(address.toLowerCase() + "000000000000000000000000");
+    } else if (isHex(address, 256)) {
+      // 32 byte hex
+      // Add raw account
+      accounts.add(address.toLowerCase());
+    } else {
+      // SS58 address
+      try {
+        accounts.add(u8aToHex(decodeAddress(address)).toLowerCase());
+      } catch (err) {
+        console.warn(`could not convert address ${address} to hex.`, err);
+      }
+    }
+  });
+  return (sourceAddress: string, beneficiaryAddress: string) =>
+    accounts.has(sourceAddress.trim().toLowerCase()) ||
+    accounts.has(beneficiaryAddress.trim().toLowerCase());
+}
