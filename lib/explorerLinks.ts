35ed51a7c2cb617c662aa12e6ef12d3dc51d29e0
Alistair Singh <alistair.singh7@gmail.com>
2025-09-09 18:29:16 +0200

    add explorer links

--- lib/explorerLinks.ts
+++ lib/explorerLinks.ts
@@ -6,6 +6,7 @@ export const EXPLORERS: { [env: string]: { [explorer: string]: string } } = {
     etherscan_11155111: "https://sepolia.etherscan.io/",
     subscan_1000: "https://assethub-paseo.subscan.io/",
     subscan_2000: "https://bridgehub-paseo.subscan.io/",
+    subscan_2043: "https://neuroweb-testnet.subscan.io/",
     subscan_relaychain: "https://paseo.subscan.io/",
     polkadot_js_2086:
       "https://polkadot.js.org/apps/?rpc=wss://peregrine.kilt.io/parachain-public-ws/",
