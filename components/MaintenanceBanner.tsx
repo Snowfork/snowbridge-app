57edfb06ec11efe87219df94a094353541e41f98
Alistair Singh <alistair.singh7@gmail.com>
2025-11-03 21:37:29 +0200

    New banner for maintenance mode

---
+++ components/MaintenanceBanner.tsx
@@ -0,0 +1,37 @@
+import { FC, PropsWithChildren } from "react";
+import { LucideHardHat } from "lucide-react";
+import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
+
+export const MaintenanceBanner: FC<PropsWithChildren> = ({ children }) => {
+  const maintenance =
+    (process.env.NEXT_PUBLIC_SHOW_MAINTENANCE ?? "false")
+      .toLowerCase()
+      .trim() === "true";
+  const message = { __html: process.env.NEXT_PUBLIC_MAINTENANCE_MSG ?? "" };
+  if (maintenance) {
+    return (
+      <>
+        <div className="rounded-lg border bg-card text-card-foreground shadow-sm w-full md:w-2/3">
+          <div className="flex flex-col gap-4 p-5">
+            <div className="self-center">
+              <LucideHardHat className="flex-none w-48 h-48" />
+            </div>
+            <p className="text-center text-xl">
+              Under Maintenance: Check back soon!
+            </p>
+            <div
+              dangerouslySetInnerHTML={message}
+              hidden={
+                (process.env.NEXT_PUBLIC_MAINTENANCE_MSG ?? "").trim()
+                  .length === 0
+              }
+              className="text-center"
+            ></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return <>{children}</>;
+};
